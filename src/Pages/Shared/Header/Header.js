8df9ca3e08c5190a9342aec5e94f3de4dfafa50e
Saadi-Rahman <sadi6280@gmail.com>
2022-11-09 13:30:53 +0600

    added navbar

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,9 +1,103 @@
 import React from 'react';
+import { Button, Container, Navbar, OverlayTrigger, Tooltip } from 'react-bootstrap';
+import { FaSignInAlt, FaSignOutAlt, FaUser, FaInstagram, FaLinkedin, FaTwitter, FaPinterest, FaFacebookSquare, FaRegEnvelope, FaYelp } from "react-icons/fa";
+import { Link, NavLink } from 'react-router-dom';
+import navLogo from '../../../assets/logo.png';
+import './Header.css';
 
 const Header = () => {
     return (
         <div>
-            <h2>This is header</h2>
+            <nav className="navbar navbar-expand-lg navbar-wide bg-body">
+                <div className="container">
+                    <Link className='pb-2' to="/"><img src={navLogo} alt="" width="200px" /></Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-wide-items navbar-nav me-auto mb-2 p-lg-0 mb-lg-0">
+                            <li className="nav-item">
+                                <NavLink className={({isActive}) => isActive ? "nav-link text-dark" : "nav-link"} to="/home">HOME</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className={({isActive}) => isActive ? "nav-link text-dark" : "nav-link"} to="services">SERVICES</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className={({isActive}) => isActive ? "nav-link text-dark" : "nav-link"} to="blog">BLOG</NavLink>
+                            </li>
+                            <li className='nav-item'>
+                                {
+                                    // user?.uid ?
+                                    // <>
+                                    //     <OverlayTrigger
+                                    //         delay={{ hide: 400, show: 200 }}
+                                    //         overlay={(props) => (
+                                    //         <Tooltip {...props}>{user?.displayName}</Tooltip>
+                                    //         )}
+                                    //         placement="bottom"
+                                    //         ><Image className='ms-2' style={{width: '35px'}} roundedCircle src={user.photoURL} alt="user"></Image>
+                                    //     </OverlayTrigger>
+                                    //     <OverlayTrigger
+                                    //         delay={{ hide: 400, show: 200 }}
+                                    //         overlay={(props) => (
+                                    //         <Tooltip {...props}>Logout</Tooltip>
+                                    //         )}
+                                    //         placement="bottom"
+                                    //         ><Button onClick={handleLogOut} variant='outline-dark mb-lg-0 ms-4'><FaSignOutAlt className='mb-1' /></Button>
+                                    //     </OverlayTrigger>
+                                    // </>
+                                    // :
+                                    <>
+                                        <OverlayTrigger
+                                            delay={{ hide: 400, show: 200 }}
+                                            overlay={(props) => (
+                                            <Tooltip {...props}>User</Tooltip>
+                                            )}
+                                            placement="bottom"
+                                            ><Button variant="ms-1 "><FaUser className='fs-4' /></Button>
+                                        </OverlayTrigger>
+                                        <OverlayTrigger
+                                            delay={{ hide: 400, show: 200 }}
+                                            overlay={(props) => (
+                                            <Tooltip {...props}>Login</Tooltip>
+                                            )}
+                                            placement="bottom"
+                                            ><Link to='/login' className='btn btn-outline-dark mb-lg-0 ms-3'><FaSignInAlt className='mb-1' /></Link>
+                                        </OverlayTrigger>
+                                    </>
+                                }
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+
+            <Navbar className='bg-warning navbar-thin'>
+                <Container>
+                    <Navbar.Text>
+                        <Link className='text-dark text-decoration-none ms-1'><FaRegEnvelope className='me-md-2' /> mr.locker@yahoo.com</Link>
+                    </Navbar.Text>
+                    <Navbar.Toggle />
+                    <Navbar.Collapse className="justify-content-end">
+                    <Navbar.Text>
+                        {/* {
+                            user?.uid ?
+                            <button onClick={handleLogOut} className="btn-logout">Logout</button>
+                            :
+                            <Link className='text-light text-decoration-none me-4' to="/login">Login</Link>
+                        } */}
+                    </Navbar.Text>
+                    <Navbar.Text>
+                        <Link className='text-dark text-decoration-none me-3'><FaFacebookSquare /></Link>
+                        <Link className='text-dark text-decoration-none me-3'><FaPinterest /></Link>
+                        <Link className='text-dark text-decoration-none me-3'><FaTwitter /></Link>
+                        <Link className='text-dark text-decoration-none me-3'><FaYelp /></Link>
+                        <Link className='text-dark text-decoration-none me-3'><FaInstagram /></Link>
+                        <Link className='text-dark text-decoration-none'><FaLinkedin /></Link>
+                    </Navbar.Text>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </div>
     );
 };

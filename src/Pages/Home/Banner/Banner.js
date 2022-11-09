aec4536f3d872db6e74a1e0b51c8f810a4652e97
Saadi-Rahman <sadi6280@gmail.com>
2022-11-09 21:35:38 +0600

    added welcome section

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Banner.css';
 
 const Banner = () => {
     return (
-        <div>
-            <h3>This is Banner</h3>
+        <div className='bg-image'>
+            <div class="mask">
+                <div class="d-flex justify-content-center align-items-center h-100">
+                    <div class="text-white">
+                    <h1 class="display-3 fw-bold mb-3">Local Emergency Locksmith <br /> Services in Charlotte NC</h1>
+                    <h5 class="mb-4">FAST, AFFORDABLE & QUALITY LOCKSMITHS 24/7</h5>
+                    <Link class="btn btn-warning fs-5 fw-semibold px-md-4 py-md-3 m-2">Request a quote</Link>
+                    <Link class="btn btn-outline-warning fs-5 fw-semibold px-md-4 py-md-3 m-2">Contact Us</Link>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -11,14 +11,14 @@ const Footer = () => {
                     <section className="">
                         <div className="row">
                             <div className="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
-                                <h6 className="fs-3 fw-bold mb-3 font-weight-bold">Mr. Locker</h6>
+                                <h6 className="fs-2 fw-bold mb-3 font-weight-bold">Mr. Locker</h6>
                                 <p>Mr. Locker Locksmith is not just highly trained, I have over 10 years of experience in the field and all the right tools to perform professional work.</p>
                             </div>
 
                             <hr className="w-100 clearfix d-md-none" />
 
                             <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
-                                <h6 className="text-uppercase mb-4 font-weight-bold">Services</h6>
+                                <h6 className="text-uppercase text-warning mb-4 font-weight-bold">Services</h6>
                                 <p><Link className="text-white text-decoration-none">Residential</Link></p>
                                 <p><Link className="text-white text-decoration-none">Automobile</Link></p>
                                 <p><Link className="text-white text-decoration-none">Emergency</Link></p>
@@ -28,7 +28,7 @@ const Footer = () => {
                             <hr className="w-100 clearfix d-md-none" />
 
                             <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
-                                <h6 className="text-uppercase mb-4 font-weight-bold">Useful links</h6>
+                                <h6 className="text-uppercase text-warning mb-4 font-weight-bold">Useful links</h6>
                                 <p><Link className="text-white text-decoration-none">Your Account</Link></p>
                                 <p><Link className="text-white text-decoration-none">Become an Affiliate</Link></p>
                                 <p><Link className="text-white text-decoration-none">Shipping Rates</Link></p>
@@ -38,8 +38,8 @@ const Footer = () => {
                             <hr className="w-100 clearfix d-md-none" />
 
                             <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
-                                <h6 className="text-uppercase mb-4 font-weight-bold">Contact</h6>
-                                <p><FaHome className='me-2' /> New York, NY 10012, US</p>
+                                <h6 className="text-uppercase text-warning mb-4 font-weight-bold">Contact</h6>
+                                <p><FaHome className='me-2' /> Charlotte, NC 10012, US</p>
                                 <p><FaEnvelope className='me-2' /> mr.locker@yahoo.com</p>
                                 <p><FaPhoneAlt className='me-2' /> + 01 234 567 88</p>
                                 <p><FaPrint className='me-2' /> + 01 234 567 89</p>
